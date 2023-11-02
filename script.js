57835e25701cd7cc5abab8a355cc82e72b42cfa6
youkwhd <lolywk@tutanota.com>
2023-11-02 16:17:30 +0700

    chore: add console info

--- script.js
+++ script.js
@@ -2,7 +2,20 @@
 
 (() => { 
     const TARGET = "Puas";
-    const SAVE = true;
+    const SAVE = false;
+
+    console.log(`
+%cy%co%cu%ck%cw%ch%cd %c<lolywk@tutanota.com>%c
+https://github.com/youkwhd/igracias-auto-survey`,
+    "background-color: #000000; color: #1a94d0",
+    "background-color: #000000; color: #01ad61",
+    "background-color: #000000; color: #7cc53e",
+    "background-color: #000000; color: #ffd23d",
+    "background-color: #000000; color: #f97c25",
+    "background-color: #000000; color: #e82c31",
+    "background-color: #000000; color: #b40be2",
+    "background-color: #000000; color: #ffffff",
+    "background-color: #000000; color: #7bf920");
 
     document
         .querySelectorAll("li>.answerlist2")
